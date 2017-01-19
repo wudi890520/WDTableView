976228930a90f1b2a274d20bb0e7bb001cafd9a3
wudi <593154883@qq.com>
2017-01-19 16:43:11 +0800

    first commit

---
+++ WDTableView/WDTableView/WDTableView.swift
@@ -0,0 +1,21 @@
+//
+//  WDTableView.swift
+//  WDTableView
+//
+//  Created by 吴頔 on 17/1/19.
+//  Copyright © 2017年 WD. All rights reserved.
+//
+
+import UIKit
+
+class WDTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
